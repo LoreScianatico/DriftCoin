39d32df06133266f6334844f3724c50c865134c9
Lorenzo <scianaticolorenzo@gmail.com>
2018-03-25 21:31:08 +0200

    Adding Tests

--- src/test/java/com/lorescianatico/driftcoin/service/BlockMinerTest.java
+++ src/test/java/com/lorescianatico/driftcoin/service/BlockMinerTest.java
@@ -1,7 +1,6 @@
 package com.lorescianatico.driftcoin.service;
 
 import com.lorescianatico.driftcoin.model.BlockChain;
-import com.lorescianatico.driftcoin.model.BlockFactory;
 import com.lorescianatico.driftcoin.repository.BlockChainRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import reactor.core.publisher.Mono;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -25,19 +25,22 @@ public class BlockMinerTest {
     @Mock
     private BlockChainRepository blockChainRepository;
 
-    private BlockChain chain;
 
     @Before
     public void setUp() {
         initMocks(this);
-        chain = BlockFactory.getBlockChain();
     }
 
     @Test
     public void mineBlock() {
 
-        when(blockChainRepository.save(any(BlockChain.class))).thenReturn(Mono.just(chain));
+        when(blockChainRepository.save(any(BlockChain.class))).thenAnswer(invocationOnMock -> {
+            BlockChain chain = invocationOnMock.getArgument(0);
+            chain.setId("A");
+            return Mono.just(chain);
+        });
         BlockChain block = miner.createChain().block();
         assertNotNull(block);
+        assertEquals("A",block.getId());
     }
 }
