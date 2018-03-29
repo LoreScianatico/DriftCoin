cd6f8eb132996182a92bf527e86d7d1c1b4c8e23
Lorenzo <scianaticolorenzo@gmail.com>
2018-03-29 22:51:51 +0200

    Adding Tests

--- src/main/java/com/lorescianatico/driftcoin/repository/BlockChainRepository.java
+++ src/main/java/com/lorescianatico/driftcoin/repository/BlockChainRepository.java
@@ -2,6 +2,8 @@ package com.lorescianatico.driftcoin.repository;
 
 import com.lorescianatico.driftcoin.model.BlockChain;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlockChainRepository extends ReactiveMongoRepository<BlockChain, String> {
 }
