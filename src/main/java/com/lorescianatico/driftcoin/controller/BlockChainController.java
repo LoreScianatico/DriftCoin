e23a90ce4e476ce7baca70f09427be788fefb138
Lorenzo <scianaticolorenzo@gmail.com>
2019-01-29 19:11:51 +0100

    Security fix.

--- src/main/java/com/lorescianatico/driftcoin/controller/BlockChainController.java
+++ src/main/java/com/lorescianatico/driftcoin/controller/BlockChainController.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
@@ -14,7 +14,7 @@ public class BlockChainController {
     @Autowired
     private BlockMiner miner;
 
-    @RequestMapping({"", "/", "/index"})
+    @GetMapping({"", "/", "/index"})
     public String createChain(Model model) {
         logger.debug("Getting Index page");
 
